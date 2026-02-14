199362bf91e39e0132f6a0e6ec1baeffee6fd6a4
eojuma <evansjuma1e@gmail.com>
2026-02-14 04:59:37 +0300

    utils

---
+++ libs/utils.go
@@ -0,0 +1,30 @@
+package libs
+
+func Abs(X int) int {
+	if X < 0 {
+		return -X
+	}
+
+	return X
+}
+
+func Normalize(blocks []Point) []Point {
+	minX := blocks[0].X
+	minY := blocks[0].Y
+
+	for _, b := range blocks {
+		if b.X < minX {
+			minX = b.X
+		}
+		if b.Y < minY {
+			minY = b.Y
+		}
+
+	}
+
+	for i := range blocks {
+		blocks[i].X -= minX
+		blocks[i].Y -= minY
+	}
+	return blocks
+}
